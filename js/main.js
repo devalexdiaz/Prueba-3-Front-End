fac6626a66c0fff4923323041cb44c2abc5d9c74
Alex <alexanderdeveloper808@gmail.com>
2023-07-11 09:15:14 -0400

    HTML listo para la implementación del js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let nombre = document.getElementById('nombre');
+let capital = document.getElementById('capital');
+let poblacion = document.getElementById('poblacion');
+let codigo = document.getElementById('codigo');
