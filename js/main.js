0cf7e7b9ef39145e8ee2a993c578d42f3a39a790
Alex <alexanderdeveloper808@gmail.com>
2023-07-11 18:23:14 -0400

    Script JS terminado

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ fetch(url)
     for (const pais in data) {
         const item = data[pais];
         const objeto={
-            "pais":item.name.common,
+            "nombre":item.name.common,
             "capital":item.capital[0],
             "poblacion":item.population,
             "codigo":item.cca3
@@ -34,7 +34,7 @@ function mostrarDatos() {
     arrayDatos.forEach((pais, index) => {
       const fila = document.createElement('tr');
       fila.innerHTML = `
-        <td>${pais.pais}</td>
+        <td>${pais.nombre}</td>
         <td>${pais.capital}</td>
         <td>${pais.poblacion}</td>
         <td>${pais.codigo}</td>
@@ -54,8 +54,103 @@ function eliminarElemento(index) {
     }
 }
 
-
+// Capturando inputs del formulario
 let nombre = document.getElementById('nombre');
 let capital = document.getElementById('capital');
 let poblacion = document.getElementById('poblacion');
 let codigo = document.getElementById('codigo');
+
+// Capturando span de mensaje de error para cada input
+let validNombre = document.getElementById('validNombre');
+let validCapital = document.getElementById('validCapital');
+let validPoblacion = document.getElementById('validPoblacion');
+let validCodigo = document.getElementById('validCodigo');
+
+// Capturando el formulario para ejecutar al hacer submit
+const formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', function(event) {
+  // Evitar recarga de página
+  event.preventDefault();
+
+  // Guardando valor de comprovación para evitar errores al mostrar span de error
+  let nombreValido = validarNombre();
+  let capitalValido = validarCapital();
+  let poblacionValida = validarPoblacion();
+  let codigoValido = validarCodigo();
+
+  // Comprovando que las validaciones esten correctas para crear el diccionario con los datos
+  if (nombreValido && capitalValido && poblacionValida && codigoValido) {
+    const datosFormulario = {
+      nombre: nombre.value,
+      capital: capital.value,
+      poblacion: poblacion.value,
+      codigo: codigo.value,
+    };
+    // Agregando los datos al array de la tabla
+    arrayDatos.push(datosFormulario);
+    mostrarDatos();
+    // Limpiando campos
+    nombre.value = '';
+    capital.value = '';
+    poblacion.value = '';
+    codigo.value = '';
+  }
+});
+
+// Funciones para validación
+
+function validarNombre() {
+  let regex = /^[A-Za-z\s]+$/;
+  if (!regex.test(nombre.value)) {
+    validNombre.style.display = "block";
+    validNombre.innerText = "Ingrese un nombre válido";
+    validNombre.className = "text-danger";
+    nombre.className = "form-control border-input-error";
+    return false;
+  }
+  nombre.className = "form-control border-input-ok";
+  validNombre.style.display = "none";
+  return true;
+}
+
+function validarCapital() {
+  let regex = /^[A-Za-z\s]+$/;
+  if (!regex.test(capital.value)) {
+    validCapital.style.display = "block";
+    validCapital.innerText = "Ingrese una capital válida";
+    validCapital.className = "text-danger";
+    capital.className = "form-control border-input-error";
+    return false;
+  }
+  capital.className = "form-control border-input-ok";
+  validCapital.style.display = "none";
+  return true;
+}
+
+function validarPoblacion() {
+  let regex = /^\d+$/;
+  if (!regex.test(poblacion.value) || parseInt(poblacion.value) < 0) {
+    validPoblacion.style.display = "block";
+    validPoblacion.innerText = "Ingrese población válida";
+    validPoblacion.className = "text-danger";
+    poblacion.className = "form-control border-input-error";
+    return false;
+  }
+  poblacion.className = "form-control border-input-ok";
+  validPoblacion.style.display = "none";
+  return true;
+}
+
+function validarCodigo() {
+  let regex = /^[A-Z]+$/;
+  if (!regex.test(codigo.value)) {
+    validCodigo.style.display = "block";
+    validCodigo.innerText = "Ingrese un codigo válido y en mayúscula";
+    validCodigo.className = "text-danger";
+    codigo.className = "form-control border-input-error";
+    return false;
+  }
+  codigo.className = "form-control border-input-ok";
+  validCodigo.style.display = "none";
+  return true;
+}
